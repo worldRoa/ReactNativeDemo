3a549fa37659b1953d896b5d5cf27fa18981bcd5
Ravon Rolle <ravonrolle@gmail.com>
2021-12-26 18:12:42 -0600

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -12,8 +12,9 @@ import { createAppContainer } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import SafeAreaView from 'react-native-safe-area-view';
 import { connect } from 'react-redux';
-import { fetchCampsites, fetchComments, fetchPromotions,
-    fetchPartners } from '../redux/ActionCreators';
+import { fetchCampsites, fetchComments, fetchPromotions,fetchPartners } from '../redux/ActionCreators';
+import Reservation from './ReservationComponent';
+
 
     const mapDispatchToProps = {
         fetchCampsites,
@@ -21,6 +22,29 @@ import { fetchCampsites, fetchComments, fetchPromotions,
         fetchPromotions,
         fetchPartners
     };
+
+    const ReservationNavigator = createStackNavigator(
+        {
+            Reservation: { screen: Reservation }
+        },
+        {
+            defaultNavigationOptions: ({navigation}) => ({
+                headerStyle: {
+                    backgroundColor: '#5637DD'
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                    color: '#fff'
+                },
+                headerLeft: <Icon
+                    name='tree'
+                    type='font-awesome'
+                    iconStyle={styles.stackIcon}
+                    onPress={() => navigation.toggleDrawer()}
+                />
+            })
+        }
+    );
 
 const AboutNavigator = createStackNavigator(
     {
@@ -169,6 +193,20 @@ const MainNavigator = createDrawerNavigator(
                 )
             }
         },
+    Reservation: {
+        screen: ReservationNavigator,
+        navigationOptions: {
+            drawerLabel: 'Reserve Campsite',
+            drawerIcon: ({tintColor}) => (
+                <Icon
+                    name='tree'
+                    type='font-awesome'
+                    size={24}
+                    color={tintColor}
+                />
+            )
+        }
+    },
         About: {
             screen: AboutNavigator,
             navigationOptions: {
