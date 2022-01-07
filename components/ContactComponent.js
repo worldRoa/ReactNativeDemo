2eaba784864b19fd32d2d4eee8c7fb8ba375738c
Ravon Rolle <ravonrolle@gmail.com>
2022-01-06 21:55:38 -0600

    Sending Email

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import { Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
+import * as MailComposer from 'expo-mail-composer';
 
 
 class Contact extends Component {
 
     static navigationOptions = {
         title: 'Contact Us'
+    }
+
+    sendMail() {
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp.co'],
+            subject: 'Inquiry',
+            body: 'To whom it may concern:'
+        })
     }
 
     render() {
@@ -31,6 +40,17 @@ class Contact extends Component {
                         {"\n"}
                         Email: campsites@nucamp.co
                     </Text>
+                    <Button
+                            title="Send Email"
+                            buttonStyle={{backgroundColor: '#5637DD', margin: 40}}
+                            icon={<Icon
+                                name='envelope-o'
+                                type='font-awesome'
+                                color='#fff'
+                                iconStyle={{marginRight: 10}}
+                            />}
+                            onPress={() => this.sendMail()}
+                        />
                     </Card>
                 </Animatable.View>
             </ScrollView>
--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -3,6 +3,7 @@ import { Text, View, ScrollView, StyleSheet,
     Picker, Switch, Button, Alert } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
 
 class Reservation extends Component {
 
@@ -40,15 +41,16 @@ class Reservation extends Component {
                     onPress: () => this.resetForm(),
                 },
                 {
-                    text: "Ok",
-                    onPress: () => this.resetForm(),
+                    text: 'OK', 
+                    onPress: () => {
+                        this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                        this.resetForm();
+                    }
                 },
             ],
-            {
-                cancelable: false,
-            }
+            { cancelable: false }
         );
-    };
+    }
 
         resetForm() {
             this.setState({
@@ -57,6 +59,31 @@ class Reservation extends Component {
                 date: new Date(),
                 showCalendar: false,
             });
+        }
+
+        async presentLocalNotification(date) {
+            function sendNotification() {
+                Notifications.setNotificationHandler({
+                    handleNotification: async () => ({
+                        shouldShowAlert: true
+                    })
+                });
+
+                Notifications.scheduleNotificationAsync({
+                    content: {
+                        title: 'Your Campsite Reservation Search',
+                        body: `Search for ${date} requested`
+                    },
+                    trigger: null
+                });
+            }
+            let permissions = await Notifications.getPermissionsAsync();
+            if (!permissions.granted) {
+                permissions = await Notifications.requestPermissionsAsync();
+            }
+            if (permissions.granted) {
+                sendNotification();
+            }
         }
 
     render() {
